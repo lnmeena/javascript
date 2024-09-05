8a22738d61a2680e68cee4757f2ce32bdaa96c87
lnmeena <149421652+lnmeena@users.noreply.github.com>
2024-09-05 14:51:54 +0000

    variables of js i learned today

---
+++ 01basics/first.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="ln@google.com"
+var accountPass="123456";
+ accountCity = "jaipur"
+ let accountState;
+ // prefer not to use var keyword instead of it use let ;
+
+
+//x=5; assignment to a constant variable is not defined
+
+ console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
+
+ 
